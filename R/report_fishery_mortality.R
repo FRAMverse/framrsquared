364fd1919399645050aca4a90e9c020196d4f09d
Ty Garber <tyler.garber@dfw.wa.gov>
2023-10-18 14:44:43 -0700

    fishery mortality screen, functionalized

---
+++ R/report_fishery_mortality.R
@@ -0,0 +1,66 @@
+#' Returns a tibble matching the Fishery Mortality screen.
+#' @param fram_db FRAM database object
+#' @param run_id Run ID
+#' @export
+#' @examples
+#' \dontrun{
+#' fram_db |> fishery_mortality(run_id = 101)
+#' }
+fishery_mortality <- function(fram_db, run_id = NULL) {
+
+  if(!is.numeric(run_id) && !is.null(run_id)) {
+    rlang::abort('Run ID must be and integer')
+  }
+
+  if(!DBI::dbIsValid(fram_db$fram_db_connection)) {
+    rlang::abort('Must connect to a FRAM database first...')
+  }
+
+  fishery_mort <- fram_db |>
+    fetch_table("Mortality") |>
+    dplyr::group_by(
+      .data$run_id,
+      .data$age,
+      .data$fishery_id,
+      .data$time_step
+    ) |>
+    dplyr::summarize(
+      dplyr::across(
+        c(
+          .data$landed_catch:.data$drop_off,
+          .data$msf_landed_catch:.data$msf_drop_off
+        ),
+        \(x) sum(x)
+      ),
+      .groups = "drop"
+    ) |>
+    tidyr::pivot_longer(.data$landed_catch:.data$msf_drop_off) |>
+    dplyr::mutate(name = stringr::str_remove(.data$name, "msf_")) |>
+    dplyr::group_by(
+      .data$run_id,
+      .data$fishery_id,
+      .data$age,
+      .data$time_step,
+      .data$name
+      ) |>
+    dplyr::summarise(value = sum(.data$value), .groups = "drop") |>
+    tidyr::pivot_wider() |>
+    dplyr::select(
+      .data$run_id:.data$time_step, .data$landed_catch,
+      .data$non_retention, .data$shaker, .data$drop_off
+    ) |>
+    dplyr::arrange(.data$run_id, .data$fishery_id, .data$age, .data$time_step)
+
+  if (is.null(run_id)) {
+    fishery_mort |> # returns pop stat for all runs in db
+      `attr<-`('species', fram_db$fram_db_species)
+  } else {
+    fishery_mort |>
+      dplyr::filter(.data$run_id %in% .env$run_id) |>
+      `attr<-`('species', fram_db$fram_db_species)
+  }
+
+
+}
+
+
