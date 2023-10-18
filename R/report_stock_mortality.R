11e30776d575f7088d7167b0df3a0867525b7a1f
Ty Garber <tyler.garber@dfw.wa.gov>
2023-10-18 16:11:31 -0700

    stock mortality screen, functionalized

--- R/report_stock_mortality.R
+++ R/report_stock_mortality.R
@@ -4,9 +4,12 @@
 #' @export
 #' @examples
 #' \dontrun{
-#' fram_db |> fishery_mortality(run_id = 101)
+#' fram_db |>
+#'  stock_mortality(run=132) |>
+#'  filter(stock_id == 17, fishery_id == 36)
+#'
 #' }
-fishery_mortality <- function(fram_db, run_id = NULL) {
+stock_mortality <- function(fram_db, run_id = NULL) {
 
   if(!is.numeric(run_id) && !is.null(run_id)) {
     rlang::abort('Run ID must be and integer')
@@ -16,12 +19,13 @@ fishery_mortality <- function(fram_db, run_id = NULL) {
     rlang::abort('Must connect to a FRAM database first...')
   }
 
-  fishery_mort <- fram_db |>
+  stock_mort <- fram_db |>
     fetch_table("Mortality") |>
     dplyr::group_by(
       .data$run_id,
       .data$age,
       .data$fishery_id,
+      .data$stock_id,
       .data$time_step
     ) |>
     dplyr::summarize(
@@ -39,10 +43,11 @@ fishery_mortality <- function(fram_db, run_id = NULL) {
     dplyr::group_by(
       .data$run_id,
       .data$fishery_id,
+      .data$stock_id,
       .data$age,
       .data$time_step,
       .data$name
-      ) |>
+    ) |>
     dplyr::summarise(value = sum(.data$value), .groups = "drop") |>
     tidyr::pivot_wider() |>
     dplyr::select(
@@ -52,15 +57,13 @@ fishery_mortality <- function(fram_db, run_id = NULL) {
     dplyr::arrange(.data$run_id, .data$fishery_id, .data$age, .data$time_step)
 
   if (is.null(run_id)) {
-    fishery_mort |> # returns fishery mortality for all runs in db
+    stock_mort |> # returns fishery mortality for all runs in db
       `attr<-`('species', fram_db$fram_db_species)
   } else {
-    fishery_mort |>
+    stock_mort |>
       dplyr::filter(.data$run_id %in% .env$run_id) |>
       `attr<-`('species', fram_db$fram_db_species)
   }
 
 
 }
-
-
