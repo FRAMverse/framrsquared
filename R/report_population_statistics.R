4aaefff8ceabb6da11e71de1c0c3633b2f77d218
Ty Garber <tyler.garber@dfw.wa.gov>
2023-10-17 17:27:32 -0700

    population statistics report

---
+++ R/report_population_statistics.R
@@ -0,0 +1,45 @@
+#' Returns a tibble matching the Population Statistics screen.
+#' @param fram_db FRAM database object
+#' @param run_id Run ID
+#' @export
+#' @examples
+#' \dontrun{fram_db |> population_statistics(run_id = 101)}
+#'
+population_statistics <- function(fram_db, run_id = NULL) {
+  cohort <- fram_db |>
+    fetch_table('Cohort') |>
+    dplyr::select(
+      .data$run_id,
+      .data$stock_id,
+      .data$age,
+      .data$time_step,
+      starting_cohort = .data$start_cohort,
+      post_nat_mort = .data$working_cohort,
+      post_pre_terminal = .data$cohort,
+      maturation = .data$mature_cohort
+    )
+
+  escapement <- fram_db |>
+    fetch_table('Escapement') |>
+    dplyr::select(-primary_key)
+
+  pop_stat <- cohort |>
+    dplyr::left_join(escapement,
+                     by = c('run_id',
+                            'stock_id',
+                            'age',
+                            'time_step')) |>
+    dplyr::mutate(
+      dplyr::across(.data$escapement, \(x) tidyr::replace_na(x, 0))
+      ) |>
+    dplyr::arrange(stock_id, time_step)
+
+  if (is.null(run_id)) {
+    pop_stat |> # returns pop stat for all runs in db
+      `attr<-`('species', fram_db$fram_db_species)
+  } else {
+    pop_stat |>
+      dplyr::filter(.data$run_id %in% .env$run_id) |>
+        `attr<-`('species', fram_db$fram_db_species)
+  }
+}
