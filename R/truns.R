ac9acdceb8bdd68c94d5931418665c2599ee495a
Ty Garber <tyler.garber@dfw.wa.gov>
2024-03-18 09:14:53 -0700

    making sure passes R CMD CHECK

--- R/truns.R
+++ R/truns.R
@@ -1,6 +1,6 @@
 #' Returns a dataframe with stocks defined
 #' by the TRuns report driver
-#' @param db_path Path to a FRAM database.
+#' @param fram_db FRAM database object
 #' @export
 #' @examples
 #' \dontrun{truns <- truns_stocks(fram_db)}
@@ -8,15 +8,15 @@
 truns_stocks <- function(fram_db){
   fram_db |>
     fetch_table('ReportDriver') |>
-    dplyr::filter(driver_name == 'PSCTRuns.DRV') |>
-    dplyr::select(stock_id = option1, stock_name = option5) |>
-    tidyr::separate_longer_delim(stock_id, ',') |>
-    dplyr::mutate(across(stock_id, as.numeric))
+    dplyr::filter(.data$driver_name == 'PSCTRuns.DRV') |>
+    dplyr::select(stock_id = .data$option1, stock_name = .data$option5) |>
+    tidyr::separate_longer_delim(.data$stock_id, ',') |>
+    dplyr::mutate(dplyr::across(.data$stock_id, as.numeric))
 }
 
 #' Returns a dataframe with fisheries defined
 #' by the TRuns report driver
-#' @param db_path Path to a FRAM database.
+#' @param fram_db FRAM database object
 #' @export
 #' @examples
 #' \dontrun{truns <- truns_fisheries(fram_db)}
@@ -24,9 +24,9 @@ truns_stocks <- function(fram_db){
 truns_fisheries <- function(fram_db){
   fram_db |>
     fetch_table('ReportDriver') |>
-    dplyr::filter(driver_name == 'PSCTRuns.DRV') |>
-    dplyr::select(fishery_id = option2, stock_name = option5) |>
-    tidyr::separate_longer_delim(fishery_id, ',') |>
-    dplyr::mutate(across(fishery_id, as.numeric))
+    dplyr::filter(.data$driver_name == 'PSCTRuns.DRV') |>
+    dplyr::select(fishery_id = .data$option2, stock_name = .data$option5) |>
+    tidyr::separate_longer_delim(.data$ishery_id, ',') |>
+    dplyr::mutate(dplyr::across(.data$fishery_id, as.numeric))
 }
 
