9fde9b68983926a1141547b98088b7835dd65ff4
Ty Garber <tyler.garber@dfw.wa.gov>
2024-03-17 21:00:58 -0700

    moved the whale to the package startup

---
+++ R/zzz.R
@@ -0,0 +1,20 @@
+.onAttach <- function(libname, pkgname) {
+  startup_message()
+}
+
+
+startup_message <- function(){
+  cli::cli_text(cat(
+    cli::col_black(
+      "
+             .
+            \":\"
+          ___:____     |\"\\/\"|
+        ,'        `.    \\  /
+        |  O        \\___/ |
+      ~^~^~^~^~^~^~^~^~^~^~^~^~
+           framrsquared 0.1
+              "
+    )
+  ))
+}
