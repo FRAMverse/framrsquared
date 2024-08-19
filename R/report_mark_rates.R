5d21ee7301d32c1e1be12e252e5d76e0f0e27f17
Ty Garber <tyler.garber@dfw.wa.gov>
2024-08-19 09:25:25 -0700

    warning about how mark rate is calculated and prepping to be merged into package

--- R/report_mark_rates.R
+++ R/report_mark_rates.R
@@ -8,41 +8,42 @@
 #' }
 coho_mark_rates <- function(fram_db, run_id=NULL) {
 
+  cli::cli_alert_warning('Coho mark rates calculated via encounters')
 
   mortality <- fram_db |>
     fetch_table('Mortality')
 
   fisheries <- fram_db |>
     fetch_table('Fishery') |>
-    dplyr::filter(species == fram_db$fram_db_species) |>
-    dplyr::select(fishery_id, fishery_name)
+    dplyr::filter(.data$species == fram_db$fram_db_species) |>
+    dplyr::select(.data$fishery_id, .data$fishery_name)
 
   runs <- fram_db |>
     fetch_table('RunID') |>
-    dplyr::select(run_id, run_year, base_period_id)
+    dplyr::select(.data$run_id, .data$run_year, .data$base_period_id)
 
   fishery_type <- fram_db |>
     fetch_table('FisheryScalers') |>
-    dplyr::select(run_id, fishery_id, time_step, fishery_flag)
-
+    dplyr::select(.data$run_id, .data$fishery_id, .data$time_step, .data$fishery_flag)
 
   mortality |>
     dplyr::inner_join(fishery_type, by = c('run_id', 'fishery_id', 'time_step')) |>
     dplyr::mutate(
-      mark = dplyr::if_else(stock_id %% 2 == 0, 'AD', 'UM'), # identify mark
+      mark = dplyr::if_else(.data$stock_id %% 2 == 0, 'AD', 'UM'), # identify mark
       # zero out encounters based on fishery flag, can probably save a few lines
       # but this is explicit
-      msf_encounters = dplyr::if_else(fishery_flag %in% c(7:8, 17:18, 27:28), msf_encounter, 0),
-      ns_encounters = dplyr::if_else(fishery_flag %in% c(1:2, 17:18, 27:28), encounter, 0)
+      msf_encounters = dplyr::if_else(.data$fishery_flag %in% c(7:8, 17:18, 27:28), .data$msf_encounter, 0),
+      ns_encounters = dplyr::if_else(.data$fishery_flag %in% c(1:2, 17:18, 27:28), .data$encounter, 0)
     ) |>
-    dplyr::select(run_id, fishery_id, stock_id, time_step, mark, msf_encounters, ns_encounters) |>
-    tidyr::pivot_longer(msf_encounters:ns_encounters) |>
-    tidyr::pivot_wider(names_from = mark, values_from = value) |>
-    dplyr::group_by(run_id, fishery_id, time_step, name) |>
-    dplyr::summarize(dplyr::across(c(AD, UM), \(x) sum(x, na.rm = TRUE)), .groups = 'drop') |>
+    dplyr::select(.data$run_id, .data$fishery_id, .data$stock_id, .data$time_step,
+                  .data$mark, .data$msf_encounters, .data$ns_encounters) |>
+    tidyr::pivot_longer(.data$msf_encounters:.data$ns_encounters) |>
+    tidyr::pivot_wider(names_from = .data$mark, values_from = .data$value) |>
+    dplyr::group_by(.data$run_id, .data$fishery_id, .data$time_step, .data$name) |>
+    dplyr::summarize(dplyr::across(c(.data$AD, .data$UM), \(x) sum(x, na.rm = TRUE)), .groups = 'drop') |>
     dplyr::mutate(
-      fishery_type = stringr::str_remove(name, '_encounters'),
-      mark_rate = AD / (AD + UM)
+      fishery_type = stringr::str_remove(.data$name, '_encounters'),
+      mark_rate = .data$AD / (.data$AD + .data$UM)
     ) |>
     #dplyr::select(run_id, fishery_id, time_step, fishery_type, mark_rate) |>
     dplyr::inner_join(runs, by='run_id') |>
