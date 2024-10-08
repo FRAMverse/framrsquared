74f1d50c3222f08a5affd587ab05a807c4e718f7
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-10-08 13:32:51 -0700

    updated fetch_table to rely on external function to define table names. Updated startup_message to conform to .onAttach best practices.

--- R/fetch_data.R
+++ R/fetch_data.R
@@ -10,59 +10,9 @@
 fetch_table <- function(fram_db, table_name = NULL){
   ## adding input checking
   validate_fram_db(fram_db)
+  all_tables = provide_table_names(is_full = TRUE)
+  limited_tables = provide_table_names(is_full = FALSE)
 
-  all_tables = c(
-    'AEQ',
-    'BackwardsFRAM',
-    'BaseCohort',
-    'BaseExploitationRate',
-    'BaseID',
-    'ChinookBaseEncounterAdjustment',
-    'ChinookBaseSizeLimit',
-    'Cohort',
-    'EncounterRateAdjustment',
-    'Escapement',
-    'Fishery',
-    'FisheryModelStockProportion',
-    'FisheryMortality',
-    'FisheryScalers',
-    'Growth',
-    'IncidentalRate',
-    'MaturationRate',
-    'Mortality',
-    'NaturalMortality',
-    'NonRetention',
-    'PSCMaxER',
-    'ReportDriver',
-    'RunEncounterRateAdjustment',
-    'RunID',
-    'ShakerMortRate',
-    'SizeLimits',
-    'SLRatio',
-    'Stock',
-    'StockFisheryRateScaler',
-    'StockRecruit',
-    'TAAETRSList',
-    'TerminalFisheryFlag',
-    'TimeStep'
-  )
-  limited_tables = c(
-    'BackwardsFRAM',
-    'BaseID',
-    'Cohort',
-    'Escapement',
-    'FisheryMortality',
-    'FisheryScalers',
-    'Mortality',
-    'NonRetention',
-    'PSCMaxER',
-    'RunID',
-    'SizeLimits',
-    'SLRatio',
-    'StockFisheryRateScaler',
-    'StockRecruit',
-    'TAAETRSList'
-  )
   if (is.null(table_name)) {
     if (fram_db$fram_db_type == 'full') {
       cli::cli_alert_info('A table name must be provided, see available options:')
@@ -78,7 +28,6 @@ fetch_table <- function(fram_db, table_name = NULL){
         padding = c(0, 1, 0, 1),
         header = cli::col_cyan("FRAM tables (full database)")
       )
-
     } else{
 
       fmt <- cli::ansi_columns(
@@ -93,7 +42,6 @@ fetch_table <- function(fram_db, table_name = NULL){
         padding = c(0, 1, 0, 1),
         header = cli::col_cyan("FRAM tables (transfer database)")
       )
-
     }
   } else{
     if (fram_db$fram_db_type == 'full') {
@@ -110,3 +58,77 @@ fetch_table <- function(fram_db, table_name = NULL){
     return(output_table)
   }
 }
+
+
+#' List names of FRAM table
+#'
+#' Provides list of FRAm database names, typically useful for internal functions.
+#'
+#' @param is_full Logical. Provide names for a full FRAM database (TRUE) or a model transfer (FALSE)?
+#'
+#' @return Character string of the names of FRAM tables
+#' @export
+#'
+#' @examples
+#' provide_table_names(is_full = FALSE)
+
+provide_table_names <- function(is_full = TRUE){
+  if(!is.logical(is_full)){
+    cli::cli_abort("`is_full` must be TRUE or FALSE, not `{is_full}`")
+  }
+ if(is_full){ ## list of possible table names from a full table
+   c('AEQ',
+     'BackwardsFRAM',
+     'BaseCohort',
+     'BaseExploitationRate',
+     'BaseID',
+     'ChinookBaseEncounterAdjustment',
+     'ChinookBaseSizeLimit',
+     'Cohort',
+     'EncounterRateAdjustment',
+     'Escapement',
+     'Fishery',
+     'FisheryModelStockProportion',
+     'FisheryMortality',
+     'FisheryScalers',
+     'Growth',
+     'IncidentalRate',
+     'MaturationRate',
+     'Mortality',
+     'NaturalMortality',
+     'NonRetention',
+     'PSCMaxER',
+     'ReportDriver',
+     'RunEncounterRateAdjustment',
+     'RunID',
+     'ShakerMortRate',
+     'SizeLimits',
+     'SLRatio',
+     'Stock',
+     'StockFisheryRateScaler',
+     'StockRecruit',
+     'TAAETRSList',
+     'TerminalFisheryFlag',
+     'TimeStep'
+   )
+ } else {
+   c(
+     'BackwardsFRAM',
+     'BaseID',
+     'Cohort',
+     'Escapement',
+     'FisheryMortality',
+     'FisheryScalers',
+     'Mortality',
+     'NonRetention',
+     'PSCMaxER',
+     'RunID',
+     'SizeLimits',
+     'SLRatio',
+     'StockFisheryRateScaler',
+     'StockRecruit',
+     'TAAETRSList'
+   )
+ }
+}
+
