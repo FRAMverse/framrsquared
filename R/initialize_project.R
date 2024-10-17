aaf65e0a0c217f819990f32a5a211ab084d3c848
cedwards-dfw <Collin.Edwards@dfw.wa.gov>
2024-10-17 16:45:55 -0700

    Updated  to pull in nicer quarto yaml files. Added to inst/

--- R/initialize_project.R
+++ R/initialize_project.R
@@ -1,8 +1,22 @@
 #'  `r lifecycle::badge("experimental")`
 #' Initializes a FRAM project
+#'
+#' By default, creates suggested folder structure from [best coding practices](https://framverse.github.io/coding-practices/), and
+#' adds WDFW-style yaml and style.css files to give quarto files consistent appearance. If you
+#' belong to another organization and want this function to support your own organization-specific quarto styling,
+#' reach out to the developers with a `_quarto.yml` and (optionally) `style.css` file.
+#'
+#' @details
+#' Dev note: new template files for additional organizations should be added to `inst/`
+#' in a subfolder matching an R-friendly organization name,
+#' and the same name should be added to the `organization` parameter description here and the `supported_organizations`
+#' in `fetch_quarto_templates()`.
+#'
+#'
 #' @param folders Vector of folders to create
-#' @param renv Boolean to initialize renv
-#' @param templates Boolean. If TRUE, copy template quarto file and style.css from snippets repo
+#' @param quarto Boolean. If TRUE, add quarto yaml file and style.css
+#' @param organization Character, defaults to "WDFW". Specifies the set of quarto templates to use. Currently only supports "WDFW".
+#' @param renv Boolean to initialize renv. Defaults to FALSE.
 #' @export
 #' @examples
 #' \dontrun{
@@ -10,8 +24,9 @@
 #' }
 initialize_project <- function(folders = c('scripts', 'original_data',
                                            'cleaned_data', 'figures',
-                                           'results'),
-                               templates = FALSE,
+                                           'results', 'results/quarto_output'),
+                               quarto = TRUE,
+                               organization = "WDFW",
                                renv = FALSE){
 
   purrr::walk(
@@ -29,23 +44,47 @@ initialize_project <- function(folders = c('scripts', 'original_data',
   }
 
 
-  if (templates){
+  if (quarto){
     cli::cli_alert_info(
-      "Copying quarto templates from the snippets repo..."
+      "Copying quarto templates"
     )
-    if(file.exists("scripts/")){
-      prefix <-  "scripts/"
-    } else {
-      prefix <-  ""
-    }
-    usethis::use_github_file("https://github.com/FRAMverse/snippets/blob/main/R/markdown-and-quarto/custom-yaml-header.Rmd",
-                             save_as = paste0(prefix, "quarto-template-header.qmd"))
-    usethis::use_github_file("https://github.com/FRAMverse/snippets/blob/main/R/markdown-and-quarto/style.css",
-                             save_as = paste0(prefix, "style.css"))
+    fetch_quarto_templates(to.path = ".",
+                           organization = organization)
 
   }
-
 
 }
 
 
+#' Creates quarto template files
+#'
+#' Creates template files from specified organization in the specified path. Generally
+#' `initialize_project()` will be more useful for new R projects, while `fetch_quarto_templates()`
+#' can be helpful when working with existing projects. See `initialize_project()`
+#' for details on adding template files for new organizations.
+#'
+#' @param to.path Destination file path for template files. Typically, root of Rproject directory.
+#' @inheritParams initialize_project
+#'
+#' @return Nothing.
+#' @export
+#'
+fetch_quarto_templates = function(to.path, organization = "WDFW"){
+  supported_organizations = c("WDFW") ## add more as appropriate.
+  ## The associated yaml and style files should be added to the `inst` folder with a subfolder
+  ## that matches the organization name
+
+  if(!organization %in% supported_organizations){
+    cli::cli_abort("Argument `organization` must be one of the supported organizations: {supported_organizations}.")
+  }
+  yaml.path = system.file(paste0(organization, "/_quarto.yml"), package = "framrsquared")
+  style.path = system.file(paste0(organization, "/style.css"), package = "framrsquared")
+  file.copy (
+    c(
+      yaml.path,
+      style.path
+    ),
+    to = to.path,
+    overwrite = FALSE
+  )
+}
