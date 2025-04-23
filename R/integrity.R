70ff07927a3041b1b33e80bfae7334098222702a
Ty Garber <tyler.garber@dfw.wa.gov>
2025-04-23 12:52:35 -0700

    echoing run id to console via run_info()

--- R/integrity.R
+++ R/integrity.R
@@ -216,6 +216,7 @@ run_info <- function(fram_db, run_id) {
   cli::cli_h1('FRAM Run Information')
   cli::cli_text(cat(cli::col_blue('Species: '), cli::col_grey(run_info$species_name[[1]])))
   cli::cli_text(cat(cli::col_blue('Database Type: '), cli::col_grey(fram_db$fram_db_type)))
+  cli::cli_text(cat(cli::col_blue('Run ID: '), cli::col_grey(run_info$run_id[[1]])))
   cli::cli_text(cat(cli::col_blue('Run Name: '), cli::col_grey(run_info$run_name[[1]])))
   cli::cli_text(cat(cli::col_blue('Run Title: '), cli::col_grey(run_info$run_title[[1]])))
   cli::cli_text(cat(cli::col_blue('Run Date: '), cli::col_grey(run_info$run_time_date[[1]])))
