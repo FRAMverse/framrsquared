ddf48160b076d84b5e190fc3facd4b8e3dcede57
cbedwards-dfw <155684316+cbedwards-dfw@users.noreply.github.com>
2026-08-11 09:52:33 -0700

    barplot and traces working perfectly

--- R/bkfram_diagnostics.R
+++ R/bkfram_diagnostics.R
@@ -69,12 +69,13 @@ aggregate_bkfram_check <- function(data){
     dplyr::mutate(stock_aggregate_id = ceiling(.data$stock_id/2),
                   stock_aggregate_name = gsub("M-", "", .data$stock_name),
                   stock_aggregate_name = gsub("U-", "", .data$stock_aggregate_name)
-                  ) |>
+    ) |>
     dplyr::summarize(escapement = sum(.data$escapement),
-              escapement_target = sum(.data$escapement_target),
-              stock_id = list(c(stock_id)),
-              stock_name = list(c(stock_name)),
-              .by = c("iteration", "stock_aggregate_name", "stock_aggregate_id")) |>
+                     escapement_target = sum(.data$escapement_target),
+                     stock_id = list(c(stock_id)),
+                     stock_name = list(c(stock_name)),
+                     missing_start_cohort = all(is.na(starting_cohort)),
+                     .by = c("iteration", "stock_aggregate_name", "stock_aggregate_id")) |>
     dplyr::mutate(esc_target_ratio = .data$escapement_target/.data$escapement)
 
   return(data = data)
@@ -82,9 +83,9 @@ aggregate_bkfram_check <- function(data){
 }
 
 
-process_bkfram_check <- function(filepath, stock_id, aggregate_stocks = TRUE){
+process_bkfram_check <- function(filepath, stock_id = NULL, aggregate_stocks = TRUE){
   validate_character(filepath)
-  validate_numeric(stock_id)
+  validate_numeric(stock_id, allow_null = TRUE)
   validate_flag(aggregate_stocks)
 
   data = parse_bkfram_check(filepath)
@@ -103,7 +104,7 @@ process_bkfram_check <- function(filepath, stock_id, aggregate_stocks = TRUE){
   } else{
 
     data <- data |>
-      dplyr::mutate(stock_label = paste0(stock_name, " (", stock_id, ")"))
+      dplyr::mutate(stock_label = paste0(.data$stock_name, " (", .data$stock_id, ")"))
 
     stock_name_lut <- data |>
       dplyr::select("stock_id", "stock_name", "stock_label") |>
@@ -111,13 +112,17 @@ process_bkfram_check <- function(filepath, stock_id, aggregate_stocks = TRUE){
 
   }
 
-  stock_label = stock_name_lut |>
-    dplyr::filter(.data$stock_id %in% .env$stock_id) |>
-    dplyr::pull(stock_label) |>
-    unique()
 
-  data <- data |>
-    dplyr::filter(.data$stock_label %in% .env$stock_label)
+  if(!is.null(stock_id)){
+    stock_label = stock_name_lut |>
+      dplyr::filter(.data$stock_id %in% .env$stock_id) |>
+      dplyr::pull(stock_label) |>
+      unique()
+
+    data <- data |>
+      dplyr::filter(.data$stock_label %in% .env$stock_label)
+  }
+
   return(data)
 }
 
@@ -145,34 +150,11 @@ plot_bkfram_convergence_trace <- function(filepath, stock_id, aggregate_stocks =
   validate_numeric(stock_id)
   validate_flag(aggregate_stocks)
 
-  data = parse_bkfram_check(filepath)
+  data = process_bkfram_check(filepath = filepath,
+                              stock_id = stock_id,
+                              aggregate_stocks = aggregate_stocks)
 
-  if(aggregate_stocks){
-    data <- aggregate_bkfram_check(data) |>
-      dplyr::rowwise() |>
-      dplyr::mutate(stock_label = paste0(.data$stock_aggregate_name, " (", paste(stock_id, collapse = ", "), ")")) |>
-      dplyr::ungroup()
-
-    stock_name_lut <- data |>
-      dplyr::select("stock_name", "stock_id", "stock_aggregate_id", "stock_aggregate_name", "stock_label") |>
-      dplyr::distinct() |>
-      tidyr::unnest(cols = c("stock_name", "stock_id"))
-
-  } else{
-
-    data <- data |>
-      dplyr::mutate(stock_label = paste0(stock_name, " (", stock_id, ")"))
-
-    stock_name_lut <- data |>
-      dplyr::select("stock_id", "stock_name", "stock_label") |>
-      dplyr::distinct()
-
-  }
-
-  stock_label = stock_name_lut |>
-    dplyr::filter(.data$stock_id %in% .env$stock_id) |>
-    dplyr::pull(stock_label) |>
-    unique()
+  stock_label = data$stock_label |> unique()
 
   if(length(stock_label) > 5){
     plot_title = glue::glue("Convergence of stocks {glue::glue_collapse(stock_id, sep = ', ')}")
@@ -181,8 +163,7 @@ plot_bkfram_convergence_trace <- function(filepath, stock_id, aggregate_stocks =
   }
 
   data |>
-    dplyr::filter(.data$stock_label %in% .env$stock_label,
-                  iteration != 1) |>
+    dplyr::filter(iteration != 1) |>
     ggplot2::ggplot(ggplot2::aes(.data$iteration, .data$escapement, col = .data$stock_label, group = .data$stock_label))+
     ggplot2::geom_line(ggplot2::aes(linetype = "Modeled")) +
     ggplot2::geom_line(ggplot2::aes(y = .data$escapement_target, linetype = "Target"))+
@@ -197,5 +178,110 @@ plot_bkfram_convergence_trace <- function(filepath, stock_id, aggregate_stocks =
     )+
     ggplot2::theme_bw(base_size = 13)
 }
+
+plot_bkfram_convergence_bar <- function(filepath,
+                                        iteration = NULL,
+                                        plot_ratio = TRUE,
+                                        aggregate_stocks = TRUE,
+                                        verbose = TRUE,
+                                        thresh = 0.01){
+
+  validate_character(filepath)
+  validate_numeric(iteration, n = 1, allow_null = TRUE)
+  validate_flag(plot_ratio)
+  validate_flag(aggregate_stocks)
+  validate_flag(verbose)
+  validate_numeric(thresh, n = 1)
+
+  data = process_bkfram_check(filepath = filepath,
+                              aggregate_stocks = aggregate_stocks)
+
+  operator <- ifelse(plot_ratio, "/", "-")
+
+  if(is.null(iteration)){
+    target_iteration = max(data$iteration)
+    x_label = glue::glue('Target Escapement {operator} Final Iteration Escapement')
+  } else {
+    target_iteration = iteration
+    x_label = glue::glue('Target Escapement {operator} Model Escapement, Iteration {target_iteration}')
+  }
+
+  if(plot_ratio){
+    gp <- plot_bkfram_convergence_bar_ratio(data = data,
+                                            target_iteration = target_iteration,
+                                            verbose = verbose,
+                                            thresh = thresh,
+                                            x_label = x_label)
+  } else {
+    gp <- plot_bkfram_convergence_bar_diff(data = data,
+                                           target_iteration = target_iteration,
+                                           verbose = verbose,
+                                           thresh = thresh,
+                                           x_label = x_label)
+  }
+  return(gp)
+}
+
+
+plot_bkfram_convergence_bar_ratio <- function(data,
+                                              target_iteration,
+                                              verbose,
+                                              thresh,
+                                              x_label){
+  data <- data |>
+    dplyr::filter(.data$iteration == target_iteration) |>
+    dplyr::filter_out(abs(esc_target_ratio - 1) < thresh) |>
+    dplyr::filter(!is.na(esc_target_ratio),
+                  !missing_start_cohort)
+
+  if(nrow(data) == 0 & verbose){
+    cli::cli_alert_success("No stocks outside of {thresh} of 1:1 ratio by iteration {target_iteration}!")
+  }
+
+  ## plotting
+  data |>
+    ggplot2::ggplot(ggplot2::aes(y = stats::reorder(.data$stock_label, .data$esc_target_ratio),
+                                 x = esc_target_ratio))+
+    ggplot2::geom_vline(xintercept = 1, linetype = 2)+
+    ggplot2::geom_col()+
+    ggplot2::theme_bw(base_size = 14) +
+    ggplot2::labs(
+      y = 'Stock',
+      x = x_label,
+      title = glue::glue("Imperfect escapement convergence, Iteration {target_iteration}"),
+      subtitle = glue::glue("Excluding stock within {thresh} of a perfect ratio")
+    )
+}
+
+plot_bkfram_convergence_bar_diff <- function(data,
+                                             target_iteration,
+                                             verbose,
+                                             thresh,
+                                             x_label){
+  data <- data |>
+    dplyr::filter(.data$iteration == target_iteration) |>
+    dplyr::mutate(escapement_diff = escapement_target - escapement) |>
+    dplyr::filter_out(abs(escapement_diff) < thresh) |>
+    dplyr::filter(!missing_start_cohort)
+
+  if(nrow(data) == 0 & verbose){
+    cli::cli_alert_success("No stocks off by more than {thresh} fish by iteration {target_iteration}!")
+  }
+
+  ## plotting
+  data |>
+    ggplot2::ggplot(ggplot2::aes(y = stats::reorder(.data$stock_label, .data$escapement_diff),
+                                 x = escapement_diff))+
+    ggplot2::geom_vline(xintercept = 1, linetype = 2)+
+    ggplot2::geom_col()+
+    ggplot2::theme_bw(base_size = 14) +
+    ggplot2::labs(
+      y = 'Stock',
+      x = x_label,
+      title = glue::glue("Imperfect escapement convergence, Iteration {target_iteration}"),
+      subtitle = glue::glue("Excluding stock within {thresh} fish of target")
+    )
+}
+
 
 
